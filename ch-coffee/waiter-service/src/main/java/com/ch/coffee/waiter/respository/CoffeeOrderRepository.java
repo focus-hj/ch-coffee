d4bd1569d330a8ace6d1378360de412ec561b8b1
huangjian606 <huangjian606@hellobike.com>
2024-01-31 19:38:15 +0800

    move

--- ch-coffee/waiter-service/src/main/java/com/ch/coffee/waiter/respository/CoffeeOrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ch.coffee.waiter.respository;
-
-import com.ch.coffee.waiter.model.CoffeeOrder;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CoffeeOrderRepository extends JpaRepository<CoffeeOrder, Long> {
-}
